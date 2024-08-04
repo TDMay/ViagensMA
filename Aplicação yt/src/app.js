455725156f936a897c5a8b03faf4f120db503404
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-08-04 13:09:05 -0300

    Adicionando imagens

--- Aplicação yt/src/app.js
+++ Aplicação yt/src/app.js
@@ -19,10 +19,31 @@ async function pegarPosts(){
         
         const div = document.createElement("div")
         const nome = document.createElement("h2")
+        const imagemUm = document.createElement("img")
+        const imagemDois = document.createElement("img")
+        const imagemTres = document.createElement("img");
+        const imagemQuatro = document.createElement("img");
 
         nome.innerText = local.nome;
+        
+        if (local.imagens && local.imagens.length > 0) {
+            imagemUm.src = local.imagens[0];
+            if (local.imagens.length > 1) {
+                imagemDois.src = local.imagens[1];
+            }
+            if (local.imagens.length > 2) {
+                imagemTres.src = local.imagens[2];
+            }
+            if (local.imagens.length > 3) {
+                imagemQuatro.src = local.imagens[3];
+            }
+        }
 
         div.appendChild(nome);
+        div.appendChild(imagemUm);
+        div.appendChild(imagemDois);
+        div.appendChild(imagemTres);
+        div.appendChild(imagemQuatro);
 
         principal.appendChild(div);
     })
