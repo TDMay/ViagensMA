29c60c7d21cd436b44688da90e1bc9a20376cabd
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2024-08-04 15:54:25 -0300

    Adicionando mapas

--- Aplicação yt/src/app.js
+++ Aplicação yt/src/app.js
@@ -4,7 +4,7 @@ const elementoCarregar = document.querySelector("#carregar")
 const principal = document.querySelector("#principal")
 
 // Pegar os posts
-async function pegarPosts(){
+async function pegarPosts() {
     const response = await fetch(url);
 
     console.log(response);
@@ -15,17 +15,18 @@ async function pegarPosts(){
 
     elementoCarregar.classList.add("esconder");
 
-    data.map((local) => {
-        
+    data.forEach((local) => {
+
         const div = document.createElement("div")
         const nome = document.createElement("h2")
         const imagemUm = document.createElement("img")
         const imagemDois = document.createElement("img")
-        const imagemTres = document.createElement("img");
-        const imagemQuatro = document.createElement("img");
+        const imagemTres = document.createElement("img")
+        const imagemQuatro = document.createElement("img")
 
         nome.innerText = local.nome;
-        
+
+        //Adicionando Imagens
         if (local.imagens && local.imagens.length > 0) {
             imagemUm.src = local.imagens[0];
             if (local.imagens.length > 1) {
@@ -39,13 +40,31 @@ async function pegarPosts(){
             }
         }
 
+        // Adicionando mapas
+        const divMapa = document.createElement("div");
+        divMapa.setAttribute("id", "map" + local._id); // Usar um ID único para cada mapa
+        divMapa.style.height = "180px";
+
+
         div.appendChild(nome);
         div.appendChild(imagemUm);
         div.appendChild(imagemDois);
         div.appendChild(imagemTres);
         div.appendChild(imagemQuatro);
+        div.appendChild(divMapa);
 
         principal.appendChild(div);
+
+        // Inicializar o mapa após adicionar o divMapa ao DOM
+        var map = L.map('map' + local._id).setView([local.latitude, local.longitude], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([local.latitude, local.longitude]).addTo(map)
+            .bindPopup(local.nome)
+            .openPopup();
     })
 }
 
